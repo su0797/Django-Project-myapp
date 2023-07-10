2d002c863cee8c7dec72e3be9e1ffdfefa0d7d94
su12 <ts04109@naver.com>
2023-07-10 15:37:17 +0900

    번역 기능 추가

--- app/settings.py
+++ app/settings.py
@@ -120,6 +120,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+LANGUAGES = [
+    ('en', 'English'),
+    ('kr', 'Korean')
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
