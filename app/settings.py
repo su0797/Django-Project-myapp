630b8d90a67522d76b42ba64ff8ca84c415736c3
su12 <ts04109@naver.com>
2023-07-10 16:18:41 +0900

    STATIC_ROOT 추가

--- app/settings.py
+++ app/settings.py
@@ -129,6 +129,7 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
